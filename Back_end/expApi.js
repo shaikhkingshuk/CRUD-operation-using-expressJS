2ac12bb08b2335e0f4229c04c1559fcf1dd84f0e
Shaikh Kingshuk <shaikhkingshuk@gmail.com>
2024-09-11 00:19:18 +0600

    performing CRUD operations

--- Back_end/expApi.js
+++ Back_end/expApi.js
@@ -14,19 +14,88 @@ app.listen(3000, () => {
 mongoose
   .connect("mongodb://localhost:27017/crudOperation")
   .then(() => {
-    console.log("connected to the database");
+    console.log("connected to the database"); //to check connected or not
   })
   .catch((error) => {
-    console.log(error);
+    console.log(error); //to see what's the error if occurs
   });
 
 //.... creating student model for DB
 
 const studentSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  class: Number,
-  section: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  class: {
+    type: Number,
+    required: true,
+  },
+  section: {
+    type: String,
+    required: true,
+  },
 });
 
 const studentModel = mongoose.model("studentModel", studentSchema);
+
+//.... doing CRUD operation with mongoDB
+
+app.post("/addStudent", async (req, res) => {
+  try {
+    const newStudent = new studentModel(req.body);
+    await newStudent.save();
+    res.status(200).send(newStudent);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.get("/students", async (req, res) => {
+  try {
+    const students = await studentModel.find();
+    res.status(200).json(students);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.get("/students/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findStudent = await studentModel.findById(id);
+    res.status(200).json(findStudent);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.put("/students/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateStudent = await studentModel.findByIdAndUpdate(id, req.body);
+    if (!updateStudent) {
+      return res.status(404).send({ message: "user not found with this ID" });
+    }
+    res.status(200).json(updateStudent);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.delete("/students/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteStudent = await studentModel.findByIdAndDelete(id);
+    if (!deleteStudent) {
+      return res.status(404).send({ message: "user not found with this ID" });
+    }
+    res.status(200).json(deleteStudent);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
